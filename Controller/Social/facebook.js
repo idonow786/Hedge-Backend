97d8018778573fd6e85af1c8aff56fc34555809f
Osama-tec <hashmiosama555@gmail.com>
2024-04-17 11:07:34 +0500

    localhost

--- Controller/Social/facebook.js
+++ Controller/Social/facebook.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const AuthFacebook = (req, res) => {
   try {
     const appId = process.env.FACEBOOK_APP_ID;
-    const redirectUri = 'https://crm-m3ck.onrender.com/auth/facebook/callback';
+    const redirectUri = 'localhost:3000/auth/facebook/callback';
     const scope = ['email', 'pages_show_list', 'pages_read_engagement', 'pages_manage_posts'].join(',');
     const authUrl = `https://www.facebook.com/v10.0/dialog/oauth?client_id=${appId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scope}`;
 
