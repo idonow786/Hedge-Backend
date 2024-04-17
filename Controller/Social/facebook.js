46d61c544a4a6aa2a31448908b17897e283fde33
Osama-tec <hashmiosama555@gmail.com>
2024-04-17 11:10:25 +0500

    now facebook

--- Controller/Social/facebook.js
+++ Controller/Social/facebook.js
@@ -4,7 +4,8 @@ const axios = require('axios');
 const AuthFacebook = (req, res) => {
   try {
     const appId = process.env.FACEBOOK_APP_ID;
-    const redirectUri = 'https://localhost:3000/auth/facebook/callback';
+    const adminId = req.adminId;
+    const redirectUri = `https://crm-m3ck.onrender.com/auth/facebook/callback?adminId=${adminId}`;
     const scope = ['email', 'pages_show_list', 'pages_read_engagement', 'pages_manage_posts'].join(',');
     const authUrl = `https://www.facebook.com/v10.0/dialog/oauth?client_id=${appId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scope}`;
 
@@ -17,8 +18,16 @@ const AuthFacebook = (req, res) => {
 
 const facebookAuthCallback = async (req, res) => {
   try {
-    const { code } = req.query;
-    const { userId, facebookId, accessToken, name, email } = req.body;
+    const { code, adminId } = req.query;
+    const appId = process.env.FACEBOOK_APP_ID;
+    const appSecret = process.env.FACEBOOK_APP_SECRET;
+    const redirectUri = `https://crm-m3ck.onrender.com/auth/facebook/callback?adminId=${adminId}`;
+
+    const tokenResponse = await axios.get(`https://graph.facebook.com/v10.0/oauth/access_token?client_id=${appId}&redirect_uri=${encodeURIComponent(redirectUri)}&client_secret=${appSecret}&code=${code}`);
+    const accessToken = tokenResponse.data.access_token;
+
+    const userResponse = await axios.get(`https://graph.facebook.com/me?fields=id,name,email&access_token=${accessToken}`);
+    const { id: facebookId, name, email } = userResponse.data;
 
     const existingUser = await FacebookUser.findOne({ facebookId });
 
@@ -28,7 +37,7 @@ const facebookAuthCallback = async (req, res) => {
       res.json({ message: 'User details updated successfully' });
     } else {
       const newUser = new FacebookUser({
-        userId,
+        userId: adminId,
         facebookId,
         accessToken,
         name,
@@ -38,27 +47,12 @@ const facebookAuthCallback = async (req, res) => {
       res.json({ message: 'User details saved successfully' });
     }
   } catch (error) {
-    console.error('Error handling user details:', error);
-    res.status(500).json({ error: 'An error occurred while handling user details' });
+    console.error('Error handling Facebook callback:', error);
+    res.status(500).json({ error: 'An error occurred while handling the Facebook callback' });
   }
 };
 
 module.exports = { facebookAuthCallback, AuthFacebook };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
