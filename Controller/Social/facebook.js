5a71b085624173b7e06adc5e682b8b9b71cee4c7
Osama-tec <hashmiosama555@gmail.com>
2024-04-17 10:50:54 +0500

    link added

--- Controller/Social/facebook.js
+++ Controller/Social/facebook.js
@@ -1,11 +1,11 @@
 const FacebookUser = require('../../Model/Facebook');
-const axios=require('axios')
+const axios = require('axios');
 
 const AuthFacebook = (req, res) => {
   try {
     const appId = process.env.FACEBOOK_APP_ID;
     const adminId = req.adminId;
-    const redirectUri = `https://crm-backend-9vvd.onrender.com/auth/facebook/callback?adminId=${adminId}`;
+    const redirectUri = `https://crm-m3ck.onrender.com/auth/facebook/callback?adminId=${adminId}`;
     const scope = ['email', 'pages_show_list', 'pages_read_engagement', 'pages_manage_posts'].join(',');
     const authUrl = `https://www.facebook.com/v10.0/dialog/oauth?client_id=${appId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scope}`;
     res.json({ authUrl });
@@ -17,10 +17,10 @@ const AuthFacebook = (req, res) => {
 
 const facebookAuthCallback = async (req, res) => {
   try {
-    const { code } = req.query;
+    const { code, adminId } = req.query;
     const appId = process.env.FACEBOOK_APP_ID;
     const appSecret = process.env.FACEBOOK_APP_SECRET;
-    const redirectUri = 'https://crm-backend-9vvd.onrender.com/auth/facebook/callback';
+    const redirectUri = `https://crm-m3ck.onrender.com/auth/facebook/callback?adminId=${adminId}`;
 
     const tokenResponse = await axios.get(`https://graph.facebook.com/v10.0/oauth/access_token?client_id=${appId}&redirect_uri=${encodeURIComponent(redirectUri)}&client_secret=${appSecret}&code=${code}`);
     const accessToken = tokenResponse.data.access_token;
@@ -28,7 +28,6 @@ const facebookAuthCallback = async (req, res) => {
     const userResponse = await axios.get(`https://graph.facebook.com/me?fields=id,name,email&access_token=${accessToken}`);
     const { id: facebookId, name, email } = userResponse.data;
 
-    const adminId = req.adminId;
     const existingUser = await FacebookUser.findOne({ facebookId });
 
     if (existingUser) {
