f567e8d127cd5f5e425ea789c8afccd99d63c816
Osama-tec <hashmiosama555@gmail.com>
2024-06-13 23:16:52 +0500

    linkedin fixing issues

--- Model/Linkedin.js
+++ Model/Linkedin.js
@@ -4,28 +4,22 @@ const mongoose = require('mongoose');
 const LinkedInUserSchema = new mongoose.Schema({
   adminId: {
     type: String,
-    required: true,
   },
   userId: {
     type: String,
-    required: true,
   },
   linkedinId: {
     type: String,
-    required: true,
     unique: true,
   },
   accessToken: {
     type: String,
-    required: true,
   },
   name: {
     type: String,
-    required: true,
   },
   email: {
     type: String,
-    required: true,
   },
   createdAt: {
     type: Date,
