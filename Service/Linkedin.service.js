8bc7e72fd3a7bb9d7d29f5bbccd587184b15221e
Osama-tec <hashmiosama555@gmail.com>
2024-06-21 21:48:41 +0500

    linkedin updated

--- Service/Linkedin.service.js
+++ Service/Linkedin.service.js
@@ -4,6 +4,57 @@ const { LinkedInUser } = require('../Model/Linkedin');
 const { uploadImageToFirebase } = require('../Firebase/uploadImage');
 const { uploadVideoToFirebase } = require('../Firebase/uploadVideo');
 
+// Function to retrieve LinkedIn user details
+const getLinkedInUserDetails = async (linkedInAccessToken) => {
+  const userDetailsUrl = 'https://api.linkedin.com/v2/me';
+  
+  const response = await axios.get(userDetailsUrl, {
+    headers: {
+      'Authorization': `Bearer ${linkedInAccessToken}`
+    }
+  });
+
+  return response.data;
+};
+
+// Function to upload media to LinkedIn
+const uploadMediaToLinkedIn = async (linkedInAccessToken, mediaUrl, mediaType, personURN) => {
+  const uploadUrl = 'https://api.linkedin.com/v2/assets?action=registerUpload';
+
+  const registerUploadData = {
+    registerUploadRequest: {
+      recipes: [`urn:li:digitalmediaRecipe:feedshare-${mediaType.toLowerCase()}`],
+      owner: personURN,
+      serviceRelationships: [
+        {
+          relationshipType: 'OWNER',
+          identifier: 'urn:li:userGeneratedContent'
+        }
+      ]
+    }
+  };
+
+  const registerUploadResponse = await axios.post(uploadUrl, registerUploadData, {
+    headers: {
+      'Authorization': `Bearer ${linkedInAccessToken}`,
+      'Content-Type': 'application/json'
+    }
+  });
+
+  const { uploadUrl: uploadDestination, asset } = registerUploadResponse.data.value;
+
+  // Upload the media to LinkedIn's storage
+  await axios.put(uploadDestination, mediaUrl, {
+    headers: {
+      'Authorization': `Bearer ${linkedInAccessToken}`,
+      'Content-Type': mediaType.startsWith('image/') ? 'image/jpeg' : 'video/mp4'
+    }
+  });
+
+  return asset;
+};
+
+// Function to create a post on LinkedIn
 const postToLinkedIn = async (adminId, title, description, mediaFiles) => {
   const linkedInUser = await LinkedInUser.findOne({ adminId });
   if (!linkedInUser) {
@@ -11,6 +62,11 @@ const postToLinkedIn = async (adminId, title, description, mediaFiles) => {
   }
 
   const linkedInAccessToken = linkedInUser.accessToken;
+
+  // Retrieve LinkedIn user details
+  const userDetails = await getLinkedInUserDetails(linkedInAccessToken);
+  const personURN = `urn:li:person:${userDetails.sub}`;
+
   const linkedInPostUrl = `https://api.linkedin.com/v2/ugcPosts`;
 
   // Upload images to Firebase Storage if any
@@ -29,27 +85,23 @@ const postToLinkedIn = async (adminId, title, description, mediaFiles) => {
     })
   );
 
-  const media = [
-    ...imageUrls.map(url => ({
+  // Upload media to LinkedIn and get URNs
+  const media = [];
+  for (const url of [...imageUrls, ...videoUrls]) {
+    const mediaType = url.includes('.mp4') ? 'VIDEO' : 'IMAGE';
+    const asset = await uploadMediaToLinkedIn(linkedInAccessToken, url, mediaType, personURN);
+    media.push({
       status: 'READY',
       description: {
         text: title
       },
-      originalUrl: url,
-      mediaType: 'IMAGE'
-    })),
-    ...videoUrls.map(url => ({
-      status: 'READY',
-      description: {
-        text: title
-      },
-      originalUrl: url,
-      mediaType: 'VIDEO'
-    }))
-  ];
+      media: asset,
+      mediaType: mediaType
+    });
+  }
 
   const linkedInPostData = {
-    author: `urn:li:person:${linkedInUser.linkedinId}`,
+    author: personURN,
     lifecycleState: 'PUBLISHED',
     specificContent: {
       'com.linkedin.ugc.ShareContent': {
@@ -65,15 +117,15 @@ const postToLinkedIn = async (adminId, title, description, mediaFiles) => {
     }
   };
 
-  const response = await axios.post(linkedInPostUrl, linkedInPostData, {
+  // Create the post on LinkedIn
+  await axios.post(linkedInPostUrl, linkedInPostData, {
     headers: {
       'Authorization': `Bearer ${linkedInAccessToken}`,
       'Content-Type': 'application/json'
     }
   });
-
-  console.log('LinkedIn response: ', response.data);
-  return response.data.id;
 };
 
-module.exports = { postToLinkedIn };
+module.exports = {
+  postToLinkedIn
+};
