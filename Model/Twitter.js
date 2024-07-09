8e44acea733679e26883a24e01977a4ca4ff8f06
Osama-tec <hashmiosama555@gmail.com>
2024-07-10 04:01:52 +0500

    twiiter

--- Model/Twitter.js
+++ Model/Twitter.js
@@ -4,44 +4,34 @@ const mongoose = require('mongoose');
 const TwitterUserSchema = new mongoose.Schema({
   adminId: {
     type: String,
-    required: true,
   },
   userId: {
     type: String,
-    required: true,
   },
   twitterId: {
     type: String,
-    required: true,
-    unique: true,
+
   },
   accessToken: {
     type: String,
-    required: true,
   },
   accessTokenSecret: {
     type: String,
-    required: true,
   },
   name: {
     type: String,
-    required: true,
   },
   session: {
     type: String,
-    required: true,
   },
   codeVerifier: {
     type: String,
-    required: true,
   },
   generatedState: {
     type: String,
-    required: true,
   },
   username: {
     type: String,
-    required: true,
   },
   createdAt: {
     type: Date,
