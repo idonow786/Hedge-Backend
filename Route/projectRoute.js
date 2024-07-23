e58b87c7c457013a9a4dedda5dca28281da9a2aa
Osama-tec <hashmiosama555@gmail.com>
2024-07-23 22:09:31 +0500

    updated

--- Route/projectRoute.js
+++ Route/projectRoute.js
@@ -40,14 +40,24 @@ router.post(
   upload.fields([
     { name: 'documentation[contracts]', maxCount: 5 },
     { name: 'documentation[permits]', maxCount: 5 },
-    { name: 'documentation[plansAndDrawings]', maxCount: 5 },
-    { name: 'documentation[reports]', maxCount: 5 },
-    { name: 'documentation[correspondence]', maxCount: 5 },
+    { name: 'documentation[plansAndDrawings]', maxCount: 10 },
+    { name: 'documentation[reports]', maxCount: 10 },
+    { name: 'documentation[correspondence]', maxCount: 10 },
     { name: 'documentation[safetyReports]', maxCount: 5 },
     { name: 'documentation[qualityReports]', maxCount: 5 },
-    { name: 'documentation[progressReports]', maxCount: 5 },
+    { name: 'documentation[progressReports]', maxCount: 10 },
     { name: 'documentation[financialReports]', maxCount: 5 },
     { name: 'documentation[environmentalReports]', maxCount: 5 },
+    { name: 'documentation[changeOrders]', maxCount: 10 },
+    { name: 'documentation[submittals]', maxCount: 10 },
+    { name: 'documentation[inspectionReports]', maxCount: 10 },
+    { name: 'documentation[meetingMinutes]', maxCount: 20 },
+    { name: 'documentation[photos]', maxCount: 50 },
+    { name: 'documentation[warranties]', maxCount: 5 },
+    { name: 'documentation[asBuiltDrawings]', maxCount: 10 },
+    { name: 'documentation[operationManuals]', maxCount: 5 },
+    { name: 'documentation[certifications]', maxCount: 5 },
+    { name: 'documentation[insuranceDocuments]', maxCount: 5 }
   ]),
   addProjectConstruction
 );
