664978de81d7ec94fea825197bc2d9f57851ee6d
Osama-tec <hashmiosama555@gmail.com>
2024-07-23 06:09:22 +0500

    updated customer

--- Controller/Customer/getCustomer.js
+++ Controller/Customer/getCustomer.js
@@ -1,4 +1,5 @@
 const Customer = require('../../Model/Customer');
+const CustomerWallet = require('../../Model/CustomerWallet');
 
 const getCustomers = async (req, res) => {
   try {
@@ -44,4 +45,81 @@ const getCustomers = async (req, res) => {
   }
 };
 
-module.exports = { getCustomers };
+
+
+const getCustomerWalletData = async (req, res) => {
+  try {
+    const { customerId } = req.body;
+    const adminId = req.adminId;
+
+    const customer = await Customer.findOne({ _id: customerId, AdminID: adminId });
+
+    if (!customer) {
+      return res.status(404).json({ message: 'Customer not found' });
+    }
+
+    let customerWallet = await CustomerWallet.findOne({ customerId, adminId })
+      .populate('expensePerProject.projectId', 'name');
+
+    const response = {
+      customerId: customer._id,
+      customerName: customer.Name,
+      customerEmail: customer.Email,
+      customerPhone: customer.PhoneNo,
+      customerCompany: customer.CompanyName,
+      customerDateJoined: customer.DateJoined,
+      customerPicUrl: customer.PicUrl,
+      totalExpense: 0,
+      totalBalance: 0,
+      expensePerProject: [],
+      yearlyBalances: [],
+      totalProjectsDone: 0,
+      totalProjectsDoing: 0,
+      monthlyPayments: []
+    };
+
+    if (customerWallet) {
+      response.totalExpense = customerWallet.totalExpense;
+      response.totalBalance = customerWallet.totalBalance;
+      response.expensePerProject = customerWallet.expensePerProject.map(exp => ({
+        projectId: exp.projectId._id,
+        projectName: exp.projectId.name,
+        amount: exp.amount
+      }));
+      response.yearlyBalances = customerWallet.yearlyBalances;
+      response.totalProjectsDone = customerWallet.totalProjectsDone;
+      response.totalProjectsDoing = customerWallet.totalProjectsDoing;
+      response.monthlyPayments = calculateMonthlyPayments(customerWallet.yearlyBalances);
+    }
+
+    res.status(200).json(response);
+
+  } catch (error) {
+    console.error('Error fetching customer wallet data:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+function calculateMonthlyPayments(yearlyBalances) {
+  const monthlyPayments = [];
+
+  yearlyBalances.forEach(yearData => {
+    yearData.monthlyBalances.forEach(monthData => {
+      monthlyPayments.push({
+        year: yearData.year,
+        month: monthData.month,
+        amount: monthData.balance
+      });
+    });
+  });
+
+  monthlyPayments.sort((a, b) => {
+    if (a.year !== b.year) return b.year - a.year;
+    return b.month - a.month;
+  });
+
+  return monthlyPayments;
+}
+
+
+module.exports = { getCustomers,getCustomerWalletData };
--- Route/customerRoute.js
+++ Route/customerRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { addCustomer } = require('../Controller/Customer/addCustomer')
 const { deleteCustomer } = require('../Controller/Customer/deleteCustomer')
 const { updateCustomer } = require('../Controller/Customer/updateCustomer')
-const { getCustomers } = require('../Controller/Customer/getCustomer')
+const { getCustomers,getCustomerWalletData } = require('../Controller/Customer/getCustomer')
 const { verifyToken } = require('../Middleware/jwt');
 const multer = require('multer');
 
@@ -25,6 +25,7 @@ router.put('/update', verifyToken, upload.fields([
   { name: 'documents', maxCount: 5 }]), updateCustomer);                      //Working
 router.delete('/remove', verifyToken, deleteCustomer);                                                //working
 router.get('/get', verifyToken, getCustomers);                                                        //working
+router.post('/get-specific', verifyToken, getCustomerWalletData);                                                        //working
 
 module.exports = router;
 
